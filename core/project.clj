8eea867e8b63a43681cbdf18473af92942be3361
riemensc <falko.riemenschneider@itemis.de>
2013-05-15 23:12:48 +0200

    Fixed project urls

--- core/project.clj
+++ core/project.clj
@@ -1,8 +1,10 @@
 (defproject domaintypes/core "1.0.0"
   :description "Defining domain data types in Clojure on the basis of records."
-  :url "https://github.com/friemen/domaintypes"
+  :url "https://github.com/friemen/domaintypes/core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :scm {:name "git"
-         :url "https://github.com/friemen/domaintypes/core"})
+        :url "https://github.com/friemen/domaintypes/core"}
+  :repositories [["clojars" {:url "https://clojars.com/repo"
+                             :creds :gpg}]])
