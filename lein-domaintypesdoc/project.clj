e6cffe97f0912f7a5a421ceab8985379adab4cda
riemensc <falko.riemenschneider@itemis.de>
2013-05-16 08:47:46 +0200

    Update versions for re-deployment

--- lein-domaintypesdoc/project.clj
+++ lein-domaintypesdoc/project.clj
@@ -1,12 +1,12 @@
-(defproject domaintypes/lein-domaintypesdoc "1.0.0"
+(defproject domaintypes/lein-domaintypesdoc "1.0.1"
   :description "Creating documentation for domain data types (a.k.a records)."
   :url "https://github.com/friemen/domaintypes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :dependencies [[net.sourceforge.plantuml/plantuml "7965"]
-                 [domaintypes/core "1.0.0"]]
+                 [domaintypes/core "1.0.1"]]
   :scm {:name "git"
-         :url "https://github.com/friemen/domaintypes"}
+         :url "https://github.com/friemen/domaintypes/lein-domaintypes"}
   :repositories [["clojars" {:url "https://clojars.com/repo"
                              :creds :gpg}]])
--- samples/project.clj
+++ samples/project.clj
@@ -1,11 +1,11 @@
-(defproject domaintypes/samples "1.0.0"
+(defproject domaintypes/samples "1.0.1"
   :description "Samples for domain types."
   :url "https://github.com/friemen/domaintypes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [domaintypes/core "1.0.0"]]
-  :plugins [[domaintypes/lein-domaintypesdoc "1.0.0"]]
+                 [domaintypes/core "1.0.1"]]
+  :plugins [[domaintypes/lein-domaintypesdoc "1.0.1"]]
   :domaintypesdoc-for [samples.projectmanagement]
   :scm {:name "git"
          :url "https://github.com/friemen/domaintypes/samples"})
